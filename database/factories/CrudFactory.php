24e9505871f135f6eb0d03a080faed4d7aafc9df
Ariful Islam Polash <arifulislampolash.25.fswad@gmail.com>
2022-09-22 17:20:41 +0600

    factory created and data insert successfully

---
+++ database/factories/CrudFactory.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace Database\Factories;
+
+use Illuminate\Database\Eloquent\Factories\Factory;
+use Illuminate\Support\Str;
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Crud>
+ */
+class CrudFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition()
+    {
+        return [
+            'name' => $this->faker->name,
+            'email' => $this->faker->email,
+            'phone' => $this->faker->phoneNumber,
+            'address' => $this->faker->address,
+            'city' => $this->faker->city,
+        ];
+    }
+}
